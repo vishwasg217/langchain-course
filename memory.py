00fa2577a1d531ceca1ddedf59fa6bddb19504b5
vishwasg217 <vishwas.g217@gmail.com>
2023-08-04 23:29:48 +0530

    chat history bug fixed

--- memory.py
+++ memory.py
@@ -1,33 +1,43 @@
-from langchain.llms import OpenAI
-from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
 
 from dotenv import dotenv_values
-import streamlit as st
-from streamlit_chat import message
-
 
 config = dotenv_values(".env")
 OPEN_AI_API = config["OPEN_AI_API"]
 ACTIVELOOP_TOKEN = config["ACTIVELOOP_TOKEN"]
 
 
-llm = OpenAI(temperature=0, openai_api_key=OPEN_AI_API, model_name="gpt-3.5-turbo")
+llm = ChatOpenAI(temperature=0, openai_api_key=OPEN_AI_API, model_name="gpt-3.5-turbo")
 
-memory = ConversationBufferMemory(return_messages=True)
+memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
 
-conversation = ConversationChain(
-    llm=llm, 
-    verbose=True, 
-    memory=memory
-)
-
-
-
+loader = PyPDFLoader("example.pdf")
+documents = loader.load()
+# split the documents into chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+# select which embeddings we want to use
+embeddings = OpenAIEmbeddings(openai_api_key=OPEN_AI_API)
+# create the vectorestore to use as the index
+db = Chroma.from_documents(texts, embeddings)
+# expose this index in a retriever interface
+retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
+# create a chain to answer questions 
+qa = ConversationalRetrievalChain.from_llm(llm, retriever, memory=memory)
+chat_history = []
 
 query = ""
-while query != "exit":
-    query = input("Enter your query: ")
-    print(conversation.predict(input=query))
-    history = memory.load_memory_variables({})['history']
-    print(history)
+
+while(query != "exit"):
+    query = input("Query: ")
+    result = qa({"question": query, "chat_history": ""})
+    print("Result: ", result)
+
+    hist = memory.load_memory_variables({})
+    print("History: ", hist)
--- qa_with_memory.py
+++ qa_with_memory.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
@@ -9,16 +9,11 @@ from langchain.embeddings import OpenAIEmbeddings
 from dotenv import dotenv_values
 import streamlit as st
 
-
 config = dotenv_values(".env")
 OPEN_AI_API = config["OPEN_AI_API"]
 ACTIVELOOP_TOKEN = config["ACTIVELOOP_TOKEN"]
 
 model = ChatOpenAI(openai_api_key=OPEN_AI_API, model_name="gpt-3.5-turbo")
-st.session_state.memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
-
-
-chat_history = []
 
 def process_text(path: str):
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
@@ -34,53 +29,45 @@ def database(splitted_text):
     db = Chroma.from_documents(splitted_text, embeddings)
     return db
 
-def conversation(db: Chroma):
-    
-    return 
 
 def handle_query(query: str):
-    result = st.session_state.conversation({"question": query})
-    history = result['chat_history']
+    result = st.session_state.conversation({"question": query, "chat_history": ""})
+    history = st.session_state.memory.load_memory_variables({})['chat_history']
     for i, msg in enumerate(history):
         if i%2 == 0:
             st.chat_message("user").write(msg.content)
         else:
             st.chat_message("assistant").write(msg.content)
 
+
 if __name__ == "__main__":
 
     if "memory" not in st.session_state:
         st.session_state.memory = None
 
-
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
-
 
     st.title("PDF Q&A")
 
     if "process_text" not in st.session_state:
         st.session_state.process_text = False
 
-
     if st.sidebar.button("Process text"):
         st.session_state.process_text = True
         with st.spinner("Processing text..."):
             splitted_text = process_text("example.pdf")
-            db = database(splitted_text)
-            # conversation(db)
-            retriever = db.as_retriever()
-            st.session_state.memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-            st.session_state.conversation = ConversationalRetrievalChain.from_llm(llm=model, retriever=retriever, memory=st.session_state.memory)
+        db = database(splitted_text)
+        # conversation(db)
+        retriever = db.as_retriever()
+        st.session_state.memory = ConversationBufferWindowMemory(memory_key='chat_history', return_messages=True, k=5)
+        st.session_state.conversation = ConversationalRetrievalChain.from_llm(llm=model, retriever=retriever, memory=st.session_state.memory)
 
 
     if st.session_state.process_text:
         query = st.chat_input("Ask a question")
         if query:
             handle_query(query)
-
-        
-        
 
 
 # what is the total number of AI publications?
